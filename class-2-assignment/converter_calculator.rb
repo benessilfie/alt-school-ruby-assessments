513aafc5777f956996bd6dee28dabad2b61f7ee9
essilfiequansah <bquansah007@gmail.com>
2022-09-09 19:17:07 +0000

    add completed assignment

--- class-2-assignment/converter_calculator.rb
+++ class-2-assignment/converter_calculator.rb
@@ -11,7 +11,7 @@ unit = gets.chomp.downcase.to_sym
 puts "\n"
 
 p "Enter the number of units you want to convert: "
-value = gets.chomp.downcase.to_i
+value = gets.chomp.to_i
 puts "\n"
 
 if unit == :w 
@@ -24,11 +24,17 @@ end
 from = gets.chomp.downcase.to_sym
 puts "\n"
 
-if unit == :w 
-    puts "Do you want to convert to kg or lb: "
-elsif unit == :l
-    puts "Do you want to convert to cm or ft: "
-else puts "Do you want to convert to °C or °F: "
+if unit == :w and from == :kg
+    puts "Do you want to convert to lb? (lb)"
+elsif unit == :w and from == :lb
+    puts "Do you want to convert to kg? (kg)"
+elsif unit == :l and from == :cm
+    puts "Do you want to convert to ft: (ft)"
+elsif unit == :l and from == :ft
+    puts "Do you want to convert to cm: (cm)"
+elsif unit == :t and from == :c
+    puts "Do you want to convert to °F: (f)"
+else puts "Do you want to convert to °C: (c)"
 end
 
 to = gets.chomp.downcase.to_sym
@@ -37,21 +43,21 @@ puts "\n"
 def convert_to(unit, value, from, to)
     if unit == :w
         if from == :kg && to == :lb
-            value * 2.2046
+            (value * 2.2046).round(2)
         elsif from == :lb && to == :kg
-            value * 0.4535
+            (value * 0.4535).round(2)
         end
     elsif unit == :l
         if from == :cm && to == :ft
-            value * 0.0328084
+            (value * 0.0328084).round(2)
         elsif from == :ft && to == :cm
-            value * 30.48
+            (value * 30.48).round(2)
         end
     else
         if from == :c && to == :f
-            value * 1.8 + 32
+            (value * 1.8 + 32).round(2)
         elsif from == :f && to == :c
-            (value - 32) / 1.8
+            ((value - 32) / 1.8).round(2)
         end
     end
 end
